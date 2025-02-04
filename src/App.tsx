4c856153777c59808fd1465210ca37227c4ff395
JaZhKa <yakoff4@gmail.com>
2025-02-04 22:42:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import ParamEditor from './components/paramEditor';
+
+const params = [
+  { id: 1, name: 'Назначение', type: 'string' },
+  { id: 2, name: 'Длина', type: 'string' },
+];
+
+const model = {
+  paramValues: [
+    { paramId: 1, value: 'повседневное' },
+    { paramId: 2, value: 'макси' },
+  ],
+};
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Param Editor</h1>
+      <ParamEditor params={params} model={model} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/paramEditor.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+interface Param {
+  id: number;
+  name: string;
+  type: string;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+const ParamEditor: React.FC<Props> = ({ params, model }) => {
+  const [paramValues, setParamValues] = useState<ParamValue[]>(
+    model.paramValues
+  );
+
+  const handleChange = (paramId: number, value: string) => {
+    setParamValues((prevValues) =>
+      prevValues.map((pv) => (pv.paramId === paramId ? { ...pv, value } : pv))
+    );
+  };
+
+  const getModel = (): Model => {
+    return { paramValues };
+  };
+
+  return (
+    <div>
+      {params.map((param) => (
+        <div key={param.id}>
+          <label>{param.name}</label>
+          <input
+            type='text'
+            value={
+              paramValues.find((pv) => pv.paramId === param.id)?.value || ''
+            }
+            onChange={(e) => handleChange(param.id, e.target.value)}
+          />
+        </div>
+      ))}
+      <button onClick={() => console.log(getModel())}>Get Model</button>
+    </div>
+  );
+};
+
+export default ParamEditor;
